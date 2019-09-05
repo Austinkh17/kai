84c212faa1b197fae12b2d65e361c6f424634a62
xukaihua <xukaihua@kedacom.com>
2019-09-05 10:18:53 +0800

    check vision

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,35 @@
 
         // eslint-disable-next-line
         console.log('-------小程序启动-------');
+        this.checkMiniProgramVersion();
+      },
+      methods:{
+        // 微信小程序版本检测
+        checkMiniProgramVersion() {
+          //检查是否存在新版本
+          wx.getUpdateManager().onCheckForUpdate(function (res) {
+            // 诉求完新版本信息的回调
+            console.log('是否有新版本：' + res.hasUpdate);
+            if(res.hasUpdate){//如果有新版本
+              // 小程序有新版本，会主动触发下载操作（无需开发者触发）
+              wx.getUpdateManager().onUpdateReady(function () {//当新版本下载完成，会进行回调
+                let timer = setTimeout(function() {
+                  wx.getUpdateManager().applyUpdate();
+                }, 5000);
+                showModal('更新提醒', '发现新版本，将自动更新。', true).then(res => {
+                  timer.clearTimeout();
+                  wx.getUpdateManager().applyUpdate();
+                }).catch(err => {
+                  console.log('更新弹框err: ', err);
+                });
+              });
+              // 小程序有新版本，会主动触发下载操作（无需开发者触发）
+              wx.getUpdateManager().onUpdateFailed(function () {//当新版本下载失败，会进行回调
+                showModal('提示', '检查到有新版本，但下载失败，请检查网络设置', true);
+              });
+            }
+          });
+        },
       }
     }
 </script>
